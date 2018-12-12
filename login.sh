e2cc5b5ce9620c80fe883e662d3c1d11f4d82a6a
m4rshall-dev <45231647+m4rshall-dev@users.noreply.github.com>
2018-12-12 12:42:10 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -6,5 +6,5 @@
 ## Copyright (c) ArtificialVPN 2018. All Rights Reserved
 ##
 
-login=$(wget "https://privatecon-vpn.com/api/?user=$username&password=$password&server=editmoto123" -q -O -)
+login=$(wget "http://45.32.124.95/api/?user=$username&password=$password&server=editmoto123" -q -O -)
 [ "$login" != '' ] && [ "$login" = "1" ] &&  echo 'authentication ok.' && exit 0 || echo 'authentication failed.'; exit 1

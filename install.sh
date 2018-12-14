f625c689ca0e264ad63e656ebf8081831bf427ae
m4rshall-dev <45231647+m4rshall-dev@users.noreply.github.com>
2018-12-14 09:11:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -83,6 +83,8 @@ sed -i "s/ipmokasito/$IP/g" /etc/squid3/squid.conf
 cd ~
 wget https://raw.githubusercontent.com/m4rshall-dev/ovpn-files/master/test.sh
 
+wget https://raw.githubusercontent.com/m4rshall-dev/ovpn-files/master/install2.sh
+chmod +x install2.sh | bash install2.sh
 
 ## Start OpenVPN Service
 service openvpn restart

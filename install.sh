8b723a0b3ad4a179a841c8a9fd246e735cef58cf
m4rshall-dev <45231647+m4rshall-dev@users.noreply.github.com>
2018-12-14 09:09:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -81,34 +81,8 @@ IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,
 sed -i "s/ipmokasito/$IP/g" /etc/squid3/squid.conf
 
 cd ~
-https://raw.githubusercontent.com/m4rshall-dev/ovpn-files/master/test.sh
+wget https://raw.githubusercontent.com/m4rshall-dev/ovpn-files/master/test.sh
 
-clear
-echo "Welcome to M4rshall's Auto Script"
-echo "for OpsPrime VPN"
-echo ""
-echo "Please type the Website IP"
-read -p "IP: " -e -i IP WebsiteIPMo
-echo ""
-echo "Please type the Server Prefix"
-read -p "Prefix: " -e -i Prefix01 ServerPrefix
-echo ""
-echo "Please type the Database Username"
-read -p "DB Username: " -e -i m4rshall DBUsername
-echo ""
-echo "Please type the Database Password"
-read -p "DB Password: " -e -i m4rshall DBPassword
-echo ""
-echo "Okay, that's all I need. We are ready to setup your Panel now"
-read -n1 -r -p "Press any key to continue..."
-
-## Configure Website
-sed -i "s/WebsiteIPMo/$WebsiteIPMo/g" ~/test.sh
-sed -i "s/ServerPrefix/$ServerPrefix/g" ~/test.sh
-sed -i "s/DBUsername/$DBUsername/g" ~/test.sh
-sed -i "s/DBPassword/$DBPassword/g" ~/test.sh
-sed -i "s/ServerPrefix/$ServerPrefix/g" /etc/openvpn/script/login.sh
-sed -i "s/WebsiteIPMo/$WebsiteIPMo/g" /etc/openvpn/script/login.sh
 
 ## Start OpenVPN Service
 service openvpn restart
